a745d8d8c33df483ba41acfee430a75cff6ba73e
Roderen <56632427+Roderen@users.noreply.github.com>
2021-02-20 14:25:50 +0200

    update

--- js/main.js
+++ js/main.js
@@ -72,4 +72,20 @@ $('.dynamic-cards').slick({
   dots: true,
   dotsClass: "dynamic-dots",
   variableWidth: true,
+    responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      }
+    },
+    {
+      breakpoint: 770,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    }
+  ]
 });
